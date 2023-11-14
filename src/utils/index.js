af4984312dd400badd6da87815822c11de04a3cc
samir-javascript <samirjavascript00@gmail.com>
2023-11-14 11:38:59 +0100

    save mode on localstorage

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,6 +9,10 @@ export const ColorModeContext = createContext()
 
 const ToggleColorMode = ({children})=> {
     const [mode,setMode] = useState('light')
+    useEffect(() => {
+      // Save the mode to localStorage whenever it changes
+      localStorage.setItem('colorMode', mode);
+    }, [mode]);
     const toggleMode = ()=> setMode((prevMode)=> prevMode === 'light' ? 'dark' : 'light')
      const theme = useMemo(()=> createTheme({
         palette: {
