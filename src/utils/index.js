e7c17ccdfca5aa413307f20eae93ba67428dbc22
samir-javascript <samirjavascript00@gmail.com>
2023-11-14 11:48:15 +0100

    save mode on local

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,31 +1,41 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
-import { useMemo , useState  ,createContext } from "react";
+import { useMemo, useState, createContext, useEffect } from "react";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+export const ColorModeContext = createContext();
 
+const ToggleColorMode = ({ children }) => {
+  // Read the mode from localStorage or default to 'light'
+  const storedMode = localStorage.getItem('colorMode');
+  const initialMode = storedMode || 'light';
 
-export const ColorModeContext = createContext()
+  const [mode, setMode] = useState(initialMode);
 
-const ToggleColorMode = ({children})=> {
-    const [mode,setMode] = useState('light')
-    useEffect(() => {
-      // Save the mode to localStorage whenever it changes
-      localStorage.setItem('colorMode', mode);
-    }, [mode]);
-    const toggleMode = ()=> setMode((prevMode)=> prevMode === 'light' ? 'dark' : 'light')
-     const theme = useMemo(()=> createTheme({
+  useEffect(() => {
+    // Save the mode to localStorage whenever it changes
+    localStorage.setItem('colorMode', mode);
+  }, [mode]);
+
+  const toggleMode = () => setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+
+  const theme = useMemo(
+    () =>
+      createTheme({
         palette: {
-            mode
-        }
-     }), [mode]);
-   return (
-     <ColorModeContext.Provider value={{mode,setMode, toggleMode}}>
-         <ThemeProvider theme={theme}>
-              {children}
-         </ThemeProvider>
-     </ColorModeContext.Provider>
-   )
-}
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={{ mode, setMode, toggleMode }}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
 export default ToggleColorMode;
+
 
