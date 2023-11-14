aff82a618c0f45dd880a29827b54f1b2a2824a3c
samir-javascript <samirjavascript00@gmail.com>
2023-11-15 00:48:23 +0100

    add serach consitional error

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,48 +1,47 @@
-import React, { useState} from 'react'
-import { useLocation } from 'react-router-dom'
-import { Search as SearchIcon } from '@mui/icons-material'
-
-import {  useDispatch } from 'react-redux'
-import { SearchContainer, SearchDiv, SearchIconWrapper, StyledInputBase} from './styles'
-import { searchMovie } from '../../features/CurrentGenreOrCategory'
-
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Search as SearchIcon } from '@mui/icons-material';
+import { useDispatch } from 'react-redux';
+import { SearchContainer, SearchDiv, SearchIconWrapper, StyledInputBase } from './styles';
+import { searchMovie } from '../../features/CurrentGenreOrCategory';
 
 export default function Search() {
-  const [query, setQuery] = useState('')
-  const { pathname} = useLocation()
-  if(pathname !== '/') return null;
-   
-  const dispatch = useDispatch()
+  const [query, setQuery] = useState('');
+  const [noResults, setNoResults] = useState(false); // New state to track no results
+  const { pathname } = useLocation();
+  if (pathname !== '/') return null;
 
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter'  && query) {
+  const dispatch = useDispatch();
+
+  const handleKeyPress = async (event) => {
+    if (event.key === 'Enter' && query) {
       try {
-        dispatch(searchMovie(query))
-         setQuery('')
+        await dispatch(searchMovie(query));
+        setQuery('');
+        setNoResults(false); // Reset noResults state on successful search
       } catch (error) {
-         console.log('something went wrong , try again')
+        console.log('Something went wrong. Please try again.');
+        setNoResults(true); // Set noResults to true on error
       }
-      
     }
-  }
+  };
 
   return (
-   <SearchContainer>
-
+    <SearchContainer>
       <SearchDiv>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search for movies"
-              inputProps={{ 'aria-label': 'search' }}
-              value={query}
-              onChange={(e)=> setQuery(e.target.value)}
-              onKeyPress={handleKeyPress}
-            />
-          </SearchDiv>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search for movies"
+          inputProps={{ 'aria-label': 'search' }}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+      </SearchDiv>
+      {noResults && <div>No movies found for {query}</div>}
     </SearchContainer>
-    
-  )
+  );
 }
 
